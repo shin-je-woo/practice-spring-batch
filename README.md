64583e778682cfcc82900765317389d05c244457
shin.jewoo <shinjw0926@naver.com>
2024-10-17 22:45:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# practice-spring-batch
+🧨 학습목적 - 스프링 배치
+
+Spring Batch 를 사용해서 배치처리 해보기
+
+## 학습 내용
+
+- Spring-Batch 환경 구성
---
+++ src/test/java/pratice/batch/PracticeBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package pratice.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
